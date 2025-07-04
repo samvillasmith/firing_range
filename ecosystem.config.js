d356519cbcf6ba98add513343185afc19861fab5
samvillasmith <samuel.villa.smith@hotmail.com>
2025-07-04 09:39:03 -0500

    Fix ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   apps: [{
-    name: "hero-arena",
-    script: "./build/index.js",
-    time: true,
-    watch: false,
-    instances: 1,
-    exec_mode: "fork",
-    env: {
-      NODE_ENV: "production",
-      PORT: process.env.PORT || 2567
-    }
+    script: "./build/index.js"
   }]
 }
